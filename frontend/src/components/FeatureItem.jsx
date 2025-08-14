9453bd2573c68bd11324f9711e26214f58cf5c4b
LucynBernard <lucie.n.bernard@hotmail.com>
2025-08-14 15:24:15 +0200

    ajout composants account et featureitem

---
+++ frontend/src/components/FeatureItem.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function FeatureItem({icon, alt, title, description}) {
+    return (<div className="feature-item">
+              <img src={icon} alt={alt} className="feature-icon" />
+              <h3 className="feature-item-title">{title}</h3>
+              <p>{description}</p>
+            </div>)
+    
+}
+
+export default FeatureItem
